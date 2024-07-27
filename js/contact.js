e3b38c011e86978ed15a3b74f0af1610824923d1
Glory <u8L1.M6h1.Q3p7.J9a2@gmail.com>
2024-07-27 13:08:48 +0300

    added council section and web-dev

--- js/contact.js
+++ js/contact.js
@@ -9,27 +9,38 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error("Error fetching staff data:", error));
 });
 
-function generateCards(container, managementCommittee) {
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://127.0.0.1:5500/data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("contactCouncil");
+      const councilCommittee = data.members.filter((member) => member.committees.includes("council"));
+      generateCards(container, councilCommittee);
+    })
+    .catch((error) => console.error("Error fetching staff data:", error));
+});
+
+function generateCards(container, committee) {
   const cardContainer = document.createElement("div");
   cardContainer.className = "container";
 
   const row = document.createElement("div");
   row.className = "row row-cols-1 row-cols-md-2 g-4 justify-content-center";
 
-  managementCommittee.forEach((member) => {
+  committee.forEach((member) => {
     const col = document.createElement("div");
     col.className = "col";
 
     const card = document.createElement("div");
-    card.className = "card mb-3 bg-transparent";
+    card.className = "card mb-3 border-0 bg-transparent";
 
     const img = document.createElement("img");
     img.src = `./../images/staff/${member.image}`;
-    img.className = "img-fluid rounded-4";
+    img.className = "img-fluid";
     img.alt = member.name;
 
     const cardBody = document.createElement("div");
-    cardBody.className = "card-body show-left rounded-4 secondary-bg text-white";
+    cardBody.className = "card-body secondary-bg text-white";
 
     const cardTitle = document.createElement("h3");
     cardTitle.className = "card-title";
@@ -41,7 +52,7 @@ function generateCards(container, managementCommittee) {
 
     // Add phone number, email, and social media links
     const phoneText = document.createElement("p");
-    phoneText.className = "card-text fs-4 mt-3";
+    phoneText.className = "card-text fs-4 mt-3 mb-1";
     phoneText.textContent = `${member.phone}`;
 
     const emailText = document.createElement("p");
