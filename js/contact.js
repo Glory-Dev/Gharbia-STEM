31f0db8df6f0beead1d9649cddb762bfd82e6bea
Glory <u8L1.M6h1.Q3p7.J9a2@gmail.com>
2024-07-27 12:27:12 +0300

    reach out section

---
+++ js/contact.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://127.0.0.1:5500/data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const container = document.getElementById("contactReachOut");
+      const managementCommittee = data.members.filter((member) => member.committees.includes("management"));
+      generateCards(container, managementCommittee);
+    })
+    .catch((error) => console.error("Error fetching staff data:", error));
+});
+
+function generateCards(container, managementCommittee) {
+  const cardContainer = document.createElement("div");
+  cardContainer.className = "container";
+
+  const row = document.createElement("div");
+  row.className = "row row-cols-1 row-cols-md-2 g-4 justify-content-center";
+
+  managementCommittee.forEach((member) => {
+    const col = document.createElement("div");
+    col.className = "col";
+
+    const card = document.createElement("div");
+    card.className = "card mb-3 bg-transparent";
+
+    const img = document.createElement("img");
+    img.src = `./../images/staff/${member.image}`;
+    img.className = "img-fluid rounded-4";
+    img.alt = member.name;
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body show-left rounded-4 secondary-bg text-white";
+
+    const cardTitle = document.createElement("h3");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = member.name;
+
+    const cardText = document.createElement("h5");
+    cardText.className = "card-text";
+    cardText.textContent = member.profession;
+
+    // Add phone number, email, and social media links
+    const phoneText = document.createElement("p");
+    phoneText.className = "card-text fs-4 mt-3";
+    phoneText.textContent = `${member.phone}`;
+
+    const emailText = document.createElement("p");
+    emailText.className = "card-text fs-4";
+    emailText.textContent = `${member.email}`;
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(phoneText);
+    cardBody.appendChild(emailText);
+
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    row.appendChild(col);
+  });
+
+  cardContainer.appendChild(row);
+  container.appendChild(cardContainer);
+}
